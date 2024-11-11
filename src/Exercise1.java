63bb0dc6ff2c87a9ad5faadf75e2175df7dd4b61
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-11 10:47:35 +0700

    Exercise 1 add comment

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,6 +1,7 @@
 import Exercise1.*;
 
 public class Exercise1 {
+    // [Thực hành] triển khai interface 'Comparable' cho các lớp hình học (v.2023)
     public static void main(String[] args) {
         Tiger tiger  = new Tiger();
         Chicken chicken = new Chicken();
