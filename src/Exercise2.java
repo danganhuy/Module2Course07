407cdd3eee3f4b6bd1f727de84875e1af6afb22f
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-11 11:34:09 +0700

    Exercise 2 finished

---
+++ src/Exercise2.java
@@ -0,0 +1,26 @@
+import Exercise2.*;
+
+import java.util.Arrays;
+
+public class Exercise2 {
+    // [Thực hành] triển khai interface 'Comparable' cho các lớp hình học
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[4];
+        circles[0] = new ComparableCircle(2);
+        circles[1] = new ComparableCircle();
+        circles[3] = new ComparableCircle(5.5, "neon", true);
+        circles[2] = new ComparableCircle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Arrays.sort(circles);
+
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
---
+++ src/Exercise2/Circle.java
@@ -0,0 +1,62 @@
+package Exercise2;
+
+public class Circle {
+    private double radius;
+    private String color;
+    private boolean filled;
+
+    public Circle() {
+        this(1, "black", false);
+    }
+
+    public Circle(double radius) {
+        this(radius, "black", false);
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
